2f04c680b3f598b8c355b3e800c9564aafd2a8d5
HairenLiao <liaohairen@gmail.com>
2012-11-13 16:12:17 +0800

    new file:   Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cstring>
+#include <cstdio>
+#include <cerrno>
+#include "sky_avl_tree.h"
+//#include "sky_hash_table.h"
+#include "sky_probing_hash_table.h"
+#include "sky_min_heap.h"
+#include "sky_sort.h"
+
+using namespace std;
+using namespace sky;
+
+int main() {
+	
+	int (*a)[10] = { 1 };
+	
+	cout << a << endl;
+	
+	a++;
+	
+//	a++;
+	
+	cout << a << endl;
+   	
+	
+	return 0;
+}
+
